2cb17adc3c0070bf71b54c132bb8538c8c9c6c58
neojato <kpaul86@gmail.com>
2015-01-03 16:06:56 -0600

    challenge 6.7

--- js/app.js
+++ js/app.js
@@ -25,6 +25,7 @@
   app.controller('ReviewController', function() {
     this.review = {};
     this.addReview = function(product) {
+      this.review.createdOn = Date.now();
       product.reviews.push(this.review);
       this.review = {};
     };
