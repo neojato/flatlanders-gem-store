12ceac586f0efd3ffb0e59a731853d02cab62c26
Kyle Paul <kpaul86@gmail.com>
2015-01-03 10:24:18 -0600

    challenge 3.2

--- js/app.js
+++ js/app.js
@@ -2,13 +2,12 @@
   var app = angular.module('gemStore', []);
 
   app.controller('StoreController', function() {
-    this.product = gem;
+    this.products = gems;
   });
 
-  var gem = {
-    name: 'Azurite',
-    price: 110.50,
-    canPurchase: false,
-    soldOut: true
-  };
+  var gems = [
+    { name: 'Azurite', price: 2.95 },
+    { name: 'Bloodstone', price: 5.95 },
+    { name: 'Zircon', price: 3.95 },
+  ];
 })();
