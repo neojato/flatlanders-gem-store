480b48df0fd2593c40a1bf9d25f2f0ab84a53044
neojato <kpaul86@gmail.com>
2015-01-03 11:23:00 -0600

    challenge 5.3

--- js/app.js
+++ js/app.js
@@ -1,6 +1,13 @@
 (function() {
   var app = angular.module('gemStore', []);
 
+  app.controller('GalleryController', function(){
+    this.current = 0;
+    this.setCurrent = function(value){
+      this.current = value ? value : 0;
+    };
+  });
+
   app.controller('StoreController', function() {
     this.products = gems;
   });
