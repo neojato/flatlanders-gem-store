0b2f4037b07b9b8faeb4bd901035144ff29da79b
neojato <kpaul86@gmail.com>
2015-01-06 20:52:19 -0600

    challenge 7.6

--- js/app.js
+++ js/app.js
@@ -21,6 +21,20 @@
     };
   });
 
+  app.directive('productGallery', function() {
+    return {
+      restrict: 'E',
+      templateUrl: 'product-gallery.html',
+      controller: function() {
+        this.current = 0;
+        this.setCurrent = function(imageNumber){
+          this.current = imageNumber || 0;
+        };
+      },
+      controllerAs: 'gallery'
+    };
+  });
+
   app.directive("productDescription", function(){
     return {
       restrict: 'E',
