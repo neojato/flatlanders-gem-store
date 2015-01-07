c8797caab9df8cc9148bc15cef1a8fb0de9626b3
neojato <kpaul86@gmail.com>
2015-01-06 23:22:59 -0600

    add missing image

--- js/app.js
+++ js/app.js
@@ -1,13 +1,6 @@
 (function() {
   var app = angular.module('gemStore', []);
 
-  app.controller('GalleryController', function(){
-    this.current = 0;
-    this.setCurrent = function(value){
-      this.current = value ? value : 0;
-    };
-  });
-
   app.controller('StoreController', function() {
     this.products = gems;
   });
