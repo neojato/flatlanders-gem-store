3d72ed6a2d9553f5653f65ce4db5a4d6058d11cd
neojato <kpaul86@gmail.com>
2015-01-08 21:15:37 -0600

    challenge 8.1

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 (function() {
-  var app = angular.module('gemStore', []);
+  var app = angular.module('gemStore', ['store-directives']);
 
   app.controller('StoreController', function() {
     this.products = gems;
@@ -11,58 +11,6 @@
       this.review.createdOn = Date.now();
       product.reviews.push(this.review);
       this.review = {};
-    };
-  });
-
-  app.directive('productGallery', function() {
-    return {
-      restrict: 'E',
-      templateUrl: 'product-gallery.html',
-      controller: function() {
-        this.current = 0;
-        this.setCurrent = function(imageNumber){
-          this.current = imageNumber || 0;
-        };
-      },
-      controllerAs: 'gallery'
-    };
-  });
-
-  app.directive("productDescription", function(){
-    return {
-      restrict: 'E',
-      templateUrl: 'product-description.html'
-    };
-  });
-
-  app.directive('productSpecs', function(){
-    return {
-      restrict: 'A',
-      templateUrl: 'product-specs.html'
-    };
-  });
-
-  app.directive("productReviews", function() {
-    return {
-      restrict: 'E',
-      templateUrl: "product-reviews.html"
-    };
-  });
-
-  app.directive("productTabs", function(){
-    return {
-      restrict: 'E',
-      templateUrl: 'product-tabs.html',
-      controller: function() {
-        this.tab = 1;
-        this.setTab = function(tab){
-          this.tab = tab;
-        };
-        this.isSet = function(tab){
-          return this.tab === tab;
-        };
-      },
-      controllerAs: 'tab'
     };
   });
 
