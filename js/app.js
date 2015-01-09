58f90cba590fb4d01b31b6664560985ab552b3d6
neojato <kpaul86@gmail.com>
2015-01-08 21:40:25 -0600

    final challenge code

--- js/app.js
+++ js/app.js
@@ -4,15 +4,17 @@
   app.controller('StoreController', ['$http',function($http){
     var store = this;
     store.products = [];
-    $http.get('/store-products.json').success(function(data) {
+    $http.get('store-products.json').success(function(data){
       store.products = data;
     });
   }]);
 
   app.controller('ReviewController', function() {
     this.review = {};
+
     this.addReview = function(product) {
       product.reviews.push(this.review);
+
       this.review = {};
     };
   });
